c0dfa091868370e47ba791a57d8523d6bfc1c7ad
Valentin Desneaux <vdesneaux@gmail.com>
2019-10-28 17:53:27 +0100

    testing calling BDD to change CSS according to heroes

--- src/Controller/QuestController.php
+++ src/Controller/QuestController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
+use App\Model\HeroesManager;
 use App\Model\StoryManager;
 
 class QuestController extends AbstractController
 {
     public function story($id)
     {
+        $heroesManager = new HeroesManager();
+        $heroes = $heroesManager->selectAll();
         $storiesManager = new StoryManager();
         $story = $storiesManager->selectOneById($id);
-        return $this->twig->render('Story/story.html.twig', ['story' => $story]);
+        return $this->twig->render('Story/story.html.twig', ['heroes'=>$heroes,'story' => $story]);
     }
 }
