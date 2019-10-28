92c4eb2984f63f64d16d47be33e0667eabdb4646
Valentin Desneaux <vdesneaux@gmail.com>
2019-10-28 11:56:38 +0100

    link to database (model/controller) to chooseHero page

---
+++ src/Model/HeroesManager.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Model;
+
+
+class HeroesManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'heroes';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+}
