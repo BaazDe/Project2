b4725981c3eb2e1bde491a5fcdb7837ae35ef88b
Sophie Fonteneau <sophie.fonteneau@outlook.fr>
2019-10-28 12:06:57 +0100

    Aucune mise à jour récupération de la page présention

--- public/index.php
+++ public/index.php
@@ -10,11 +10,3 @@ require_once __DIR__ . '/../config/debug.php';
 require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../src/routing.php';
-
-
-
-
-
-//$charcaterWarrior = new \App\Controller\CharactersManager('Warrior', 130,'Axe', 150, 'Veritaserum', 12);
-
-//var_dump($charcaterWarrior);
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -23,4 +23,9 @@ class HomeController extends AbstractController
     {
         return $this->twig->render('Home/index.html.twig');
     }
+
+    public function choose()
+    {
+        return $this->twig->render('Home/index.html.twig');
+    }
 }
