7a16b534f67d3268c577d542b36668d699d86c85
Valentin Desneaux <vdesneaux@gmail.com>
2019-10-29 17:12:46 +0100

    adding inventoryManager

--- src/Model/InventoryManager.php
+++ src/Model/InventoryManager.php
@@ -19,35 +19,35 @@ class InventoryManager extends AbstractManager
         parent::__construct(self::TABLE);
     }
 
-    public function selectWeapons(int $id_hero)
+    public function selectWeapons(int $idHero)
     {
         $statement = $this->pdo->prepare("select w.name, w.attack from inventory
     JOIN heroes as h on inventory.heroes_id = h.id
     JOIN weapons as w on w.id = inventory.weapons_id
-where h.id = :id_hero" );
-        $statement->bindValue('id_hero', $id_hero, \PDO::PARAM_INT);
+where h.id = :id_hero");
+        $statement->bindValue('id_hero', $idHero, \PDO::PARAM_INT);
         $statement->execute();
         return $statement->fetchAll();
     }
 
-    public function selectSpells(int $id_hero)
+    public function selectSpells(int $idHero)
     {
         $statement = $this->pdo->prepare("select s.name, s.attack from inventory
     JOIN heroes as h on inventory.heroes_id = h.id
     JOIN spells as s on s.id = inventory.spells_id
-where h.id = :id_hero" );
-        $statement->bindValue('id_hero', $id_hero, \PDO::PARAM_INT);
+where h.id = :id_hero");
+        $statement->bindValue('id_hero', $idHero, \PDO::PARAM_INT);
         $statement->execute();
         return $statement->fetchAll();
     }
 
-    public function selectPotions(int $id_hero)
+    public function selectPotions(int $idHero)
     {
         $statement = $this->pdo->prepare("select p.name, p.regen from inventory
     JOIN heroes as h on inventory.heroes_id = h.id
     JOIN potions as p on p.id = inventory.potions_id
-where h.id = :id_hero" );
-        $statement->bindValue('id_hero', $id_hero, \PDO::PARAM_INT);
+where h.id = :id_hero");
+        $statement->bindValue('id_hero', $idHero, \PDO::PARAM_INT);
         $statement->execute();
         return $statement->fetchAll();
     }
