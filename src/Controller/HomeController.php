8ac3a63c1da948d3dd0b6b4b980c23ee6fd3bb0a
Valentin Desneaux <vdesneaux@gmail.com>
2019-10-29 08:54:46 +0100

    method resetting heroes stats to origin when visiting chooseHero page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,7 +14,9 @@ class HomeController extends AbstractController
 
     public function choose()
     {
+
         $heroesManager = new HeroesManager();
+        $heroesManager->resetHeroes();
         $heroes = $heroesManager->selectAll();
         return $this->twig->render('Begin/chooseHero.html.twig', ['heroes' => $heroes]);
     }
--- src/Model/HeroesManager.php
+++ src/Model/HeroesManager.php
@@ -3,7 +3,6 @@
 
 namespace App\Model;
 
-
 class HeroesManager extends AbstractManager
 {
     /**
@@ -17,5 +16,17 @@ class HeroesManager extends AbstractManager
     public function __construct()
     {
         parent::__construct(self::TABLE);
+    }
+
+    /**
+     * @return string
+     */
+    public function resetHeroes():string
+    {
+        // prepared request
+        $statement = $this->pdo->exec("UPDATE $this->table SET health = 130, attack = 150, gold = 20 WHERE id = 1");
+        $statement2 = $this->pdo->exec("UPDATE $this->table SET health = 80, attack = 200, gold = 20 WHERE id = 2");
+        $statement3 = $this->pdo->exec("UPDATE $this->table SET health = 100, attack = 165, gold = 40 WHERE id = 3");
+        return $statement.$statement2.$statement3;
     }
 }
