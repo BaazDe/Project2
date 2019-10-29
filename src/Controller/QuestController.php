40298050e373713685701c6b78c046593752b55c
Valentin Desneaux <vdesneaux@gmail.com>
2019-10-29 10:55:01 +0100

    dynamic stylesheet in story page according to choice of hero

--- src/Controller/QuestController.php
+++ src/Controller/QuestController.php
@@ -7,12 +7,29 @@ use App\Model\StoryManager;
 
 class QuestController extends AbstractController
 {
+
+    public function requestPath()
+    {
+        $requestUri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+        $scriptName = explode('/', trim($_SERVER['SCRIPT_NAME'], '/'));
+        $parts = array_diff_assoc($requestUri, $scriptName);
+        if (empty($parts)){
+            return '/';
+        }
+        $path = implode('/', $parts);
+        if (($position = strpos($path, '?')) !== FALSE)
+        {
+            $path = substr($path, 0, $position);
+        }
+        return $parts;
+    }
+
     public function story($id)
     {
         $heroesManager = new HeroesManager();
         $heroes = $heroesManager->selectAll();
         $storiesManager = new StoryManager();
         $story = $storiesManager->selectOneById($id);
-        return $this->twig->render('Story/story.html.twig', ['heroes'=>$heroes,'story' => $story]);
+        return $this->twig->render('Story/story.html.twig', ['heroes'=>$heroes,'story' => $story, 'path'=>$this->requestPath()]);
     }
 }
