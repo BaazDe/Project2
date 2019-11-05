2670bedad78a74897bf9d0f237fa16f6488c510a
Orianne Tanguy <otanguy28@gmail.com>
2019-11-05 10:45:33 +0100

    avancement du responsive + formulaire

--- src/Controller/EnigmaController.php
+++ src/Controller/EnigmaController.php
@@ -8,10 +8,22 @@ use App\Model\StoryManager;
 
 class EnigmaController extends AbstractController
 {
+    public function requestPath()
+    {
+        $requestUri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+        $scriptName = explode('/', trim($_SERVER['SCRIPT_NAME'], '/'));
+        $parts = array_diff_assoc($requestUri, $scriptName);
+        if (empty($parts)) {
+            return '/';
+        }
+        return $parts;
+    }
+
     public function enigma1($id)
     {
         $enigmasManager = new EnigmasManager();
         $enigme = $enigmasManager->selectOneById($id);
         return $this->twig->render('Enigmas/enigma1.html.twig', ['enigme' => $enigme]);
     }
+
 }
