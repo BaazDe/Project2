079d60e72215fd6eab8d98df3bf6b5dcb5ff83e7
Sophie Fonteneau <sophie.fonteneau@outlook.fr>
2019-11-08 17:58:29 +0100

    Suppression deadmanager optimisation page mort dans storyController

--- src/Controller/QuestController.php
+++ src/Controller/QuestController.php
@@ -50,6 +50,8 @@ class QuestController extends AbstractController
         $locationsManager = new LocationManager();
         $location=$locationsManager->selectOneById($locationId);
         $location=$location['name'];
+
+        // Path
         return $this->twig->render('Story/story.html.twig', [
             'potions' => $potions,
             'weapons'=>$weapons,
@@ -71,5 +73,11 @@ class QuestController extends AbstractController
         $potionsManager->deletePotion();
         //header on the page where the potion was used
         header('Location: ' . $_SERVER['HTTP_REFERER']);
+    }
+
+    // send to dead page
+    public function end()
+    {
+        return $this->twig->render('Story/dead.html.twig');
     }
 }
