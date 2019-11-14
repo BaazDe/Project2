b7381a860ac46516b7872bfd29f4f915c0fd82a5
Nathan Vanpeene <nathv59@gmail.com>
2019-11-14 09:52:54 +0100

    correction du beug d'affichage du sac en combat responsive

--- public/assets/JS/bagScript.js
+++ public/assets/JS/bagScript.js
@@ -5,6 +5,7 @@ const history = document.getElementById("history");
 const responses = document.getElementById("responses");
 const enigma = document.getElementById("enigma");
 const displayBag = document.getElementById("bagScript");
+const combat = document.getElementById("combat");
 
 if (history !== null) {
     bagIcon.addEventListener("click", bagScript);
@@ -23,6 +24,16 @@ if (enigma !==null) {
     {
         displayBag.classList.toggle("script");
         enigma.classList.toggle("script");
+        main.classList.toggle("script");
+    }
+}
+
+if (combat !==null) {
+    bagIcon.addEventListener("click", bagScriptCombat);
+    function bagScriptCombat()
+    {
+        displayBag.classList.toggle("script");
+        combat.classList.toggle("script");
         main.classList.toggle("script");
     }
 }
